0756357b3eb8fd5ced6e719d4b2cc20f6d0bd94c
peng0716 <284393960@qq.com>
2016-08-08 11:45:36 +0800

    更新

--- js/detail.js
+++ js/detail.js
@@ -56,6 +56,73 @@ $(function(){
     });
 });
 
+/*饼图*/
+var myPie = echarts.init(document.getElementById('myPie'));
+var option = {
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b}: {c} ({d}%)"
+    },
+    legend: {
+        left:0,
+        bottom:0,
+        itemWidth:10,
+        itemHeight:10,
+        textStyle:{
+            fontSize:10,
+        },
+        data:['直达','营销广告','搜索引擎','邮件营销','联盟广告','视频广告','百度','谷歌','必应','其他']
+    },
+    series: [
+        {
+            name:'访问来源',
+            type:'pie',
+            selectedMode: 'single',
+            selectedOffset:7,
+            radius: [0, '35%'],
+            center: ['50%', '40%'],
+            label: {
+                normal: {
+                    position: 'inner',
+                    fontSIze:10,
+                }
+            },
+            labelLine: {
+                normal: {
+                    show: false,
+                }
+            },
+            data:[
+                {value:335, name:'直达', selected:true},
+                {value:679, name:'营销广告'},
+                {value:1548, name:'搜索引擎'}
+            ]
+        },
+        {
+            name:'访问来源',
+            type:'pie',
+            radius: ['50%', '65%'],
+            center: ['50%', '40%'],
+            labelLine: {
+                normal: {
+                    length:10,
+                    length2:5,
+                }
+            },
+            data:[
+                {value:335, name:'直达'},
+                {value:310, name:'邮件营销'},
+                {value:234, name:'联盟广告'},
+                {value:135, name:'视频广告'},
+                {value:1048, name:'百度'},
+                {value:251, name:'谷歌'},
+                {value:147, name:'必应'},
+                {value:102, name:'其他'}
+            ]
+        }
+    ]
+};
+myPie.setOption(option);
 
 /*折线图*/
 var legendLine = echarts.init(document.getElementById('legendLine'),'infographic');
@@ -165,13 +232,12 @@ var option1 = {
 legendLine.setOption(option1);
 legendLine.hideLoading();
 
+
 /*柱状图 - 饼图*/
 var legendBar = echarts.init(document.getElementById('legendBar'),'infographic');
 legendBar.showLoading({
     text:'正在努力加载中...'
 })
-
-
 var option2 = {
     baseOption: {
         timeline: {
